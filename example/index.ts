1afa9a61e48dbd95d3a618123cd50953d2c99e99
SirDarquan <sirdarquan@gmail.com>
2016-06-12 20:08:58 -0500

    removed router because it's not being used

--- example/index.ts
+++ example/index.ts
@@ -1,7 +1,6 @@
 import {bootstrap} from "@angular/platform-browser-dynamic";
 import { provide } from '@angular/core';
 import {TodoApp} from "./app/index";
-import {ROUTER_PROVIDERS} from "@angular/router-deprecated";
 import {BROWSER_STORAGE_PROVIDERS} from  "../src/index";
 
-bootstrap(TodoApp, [ROUTER_PROVIDERS, BROWSER_STORAGE_PROVIDERS]);
+bootstrap(TodoApp, [BROWSER_STORAGE_PROVIDERS]);
