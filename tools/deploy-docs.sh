1256c60308fec4995ced593c115f986fa57fb56a
SirDarquan <sirdarquan@gmail.com>
2016-11-29 19:44:39 -0600

    bump

--- tools/deploy-docs.sh
+++ tools/deploy-docs.sh
@@ -31,6 +31,7 @@ cd docs
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+git add .
 #If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
     echo "No changes to the output on this push; exiting."
@@ -39,7 +40,6 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
