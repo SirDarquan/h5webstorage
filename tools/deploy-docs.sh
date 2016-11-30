8981ff6cf01009231ff7cfb1fda80b67d55c80c3
SirDarquan <sirdarquan@gmail.com>
2016-11-29 21:55:44 -0600

    bump

--- tools/deploy-docs.sh
+++ tools/deploy-docs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 # Save some useful information
@@ -17,12 +16,21 @@ cd docs
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-# Clean out existing contents
-#rm -rf docs/**/* || exit 0
-
 # Copy contents from ./SirDarquan/h5webstorage/dist/docs
 cd h5webstorage
 npm run documentation
+
+# If there are no changes to the documentation then just bail.
+if [[-z $(git status --porcelain) ]]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Get the deploy key by using stored variables to decrypt deploy.enc
+openssl aes-256-cbc -pass "pass:$DEPLOY_PASS" -in ./tools/deploy.enc -out ./tools/deploy_key -d -a
+chmod 600 ./tools/deploy_key
+eval `ssh-agent -s`
+ssh-add ./tools/deploy_key
 
 # Now let's go have some fun with the cloned repo
 cd ..
@@ -31,25 +39,11 @@ cd docs
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-#If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [[-z $(git status --porcelain) ]]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-cat > deploy_key <<EOF 
-ssh-rsa $DEPLOY_KEY $COMMIT_AUTHOR_EMAIL 
-EOF
-
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
