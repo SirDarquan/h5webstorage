b3ead2a487eb4746d975ccf2f0b13229f77b968b
SirDarquan <sirdarquan@gmail.com>
2016-11-29 19:33:54 -0600

    bump

--- tools/deploy-docs.sh
+++ tools/deploy-docs.sh
@@ -18,7 +18,7 @@ git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
 # Clean out existing contents
-rm -rf docs/**/* || exit 0
+#rm -rf docs/**/* || exit 0
 
 # Copy contents from ./SirDarquan/h5webstorage/dist/docs
 cd h5webstorage
@@ -30,11 +30,11 @@ cd
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-# if [ -z `git diff --exit-code` ]; then
-#     echo "No changes to the output on this push; exiting."
-#     exit 0
-# fi
+If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
