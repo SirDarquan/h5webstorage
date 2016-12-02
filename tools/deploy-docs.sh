171f18a53805b98d4d457754548fef7bb990ef8b
SirDarquan <sirdarquan@gmail.com>
2016-12-01 18:51:27 -0600

    bump

--- tools/deploy-docs.sh
+++ tools/deploy-docs.sh
@@ -27,7 +27,7 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 # Get the deploy key by using stored variables to decrypt deploy.enc
-openssl aes-256-cbc -pass "pass:$DEPLOY_PASS" -in ./tools/deploy.enc -out ./tools/deploy_key -d -a
+openssl aes-256-cbc -pass "env:$DEPLOY_PASS" -in ./tools/deploy.enc -out ./tools/deploy_key -d -a
 chmod 600 ./tools/deploy_key
 eval `ssh-agent -s`
 ssh-add ./tools/deploy_key
