6469499b496c9105d487acad4969cc8fb0dd6808
SirDarquan <sirdarquan@gmail.com>
2016-12-13 21:07:05 -0600

    possible deployment failure

--- tools/deployment.sh
+++ tools/deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-node ./tools/setupDeploy 
+node ./tools/setupDeploy.js 
 
 if [[ $TRAVIS_TAG ~= [0-9]+\.[0-9]+\.[0-9]+(-([a-z]+)\.[0-9]+)? ]];
 then
